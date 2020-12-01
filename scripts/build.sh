84fef06348916f94b96c79bbf2d5b6fc8474d80b
David Mella <david3e05@gmail.com>
2020-12-01 18:45:45 -0500

    added primative and broken build without cmake

---
+++ scripts/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+mkdir -p bin
+cd bin
+mkdir -p game
+
+
+if [ "$1" = "build" ]
+then
+	cd game
+#	cmake kjfrhdsklfhf				doesnt work and has nothing to build
+#	make						cmake machine broke :(
+	echo "cmake machine broke :("
+fi
